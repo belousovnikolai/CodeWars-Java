52e2d4b98e49faebd4d1a077067f14847916d440
nikolai belousov <belousov.nikolai@gmail.com>
2020-04-10 19:23:21 +0300

    Added solution for Responsible Drinking kata

---
+++ src/main/java/com/codewars/ResponsibleDrinking.java
@@ -0,0 +1,20 @@
+package com.codewars;
+
+public class ResponsibleDrinking {
+
+    public static String hydrate(String drinkString) {
+
+        String[] drinks = drinkString.split(",");
+        int amount = 0;
+
+        for (String drink : drinks) {
+            amount += Integer.parseInt(drink.replaceAll("[^0-9]", ""));
+        }
+
+        if (amount == 1) {
+            return amount + " glass of water";
+        }
+
+        return amount + " glasses of water";
+    }
+}
