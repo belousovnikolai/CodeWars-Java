5b51e17d1f947e60912da8c48d229527f8a16670
nikolai belousov <belousov.nikolai@gmail.com>
2020-04-11 00:23:36 +0300

    Added PredictYourAge implementation

---
+++ src/main/java/com/codewars/kyu7/PredictYourAge.java
@@ -0,0 +1,14 @@
+package com.codewars.kyu7;
+
+public class PredictYourAge {
+    public static int predictAge(int age1, int age2, int age3, int age4, int age5, int age6, int age7, int age8) {
+        int[] ages = { age1, age2, age3, age4, age5, age6, age7, age8 };
+        int sum = 0;
+
+        for (int age : ages) {
+            sum += Math.pow(age, 2);
+        }
+
+        return (int) Math.sqrt(sum) / 2;
+    }
+}
