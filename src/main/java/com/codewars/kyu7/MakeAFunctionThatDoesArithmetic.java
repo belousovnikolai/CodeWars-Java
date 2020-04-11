9d6ca3cc1488dc1eee0b2eecb2a6b038c28c6b05
nikolai belousov <belousov.nikolai@gmail.com>
2020-04-12 02:02:55 +0300

    Added solution for MakeAFunctionThatDoesArithmetic kata

---
+++ src/main/java/com/codewars/kyu7/MakeAFunctionThatDoesArithmetic.java
@@ -0,0 +1,22 @@
+package com.codewars.kyu7;
+
+public class MakeAFunctionThatDoesArithmetic {
+    public static int arithmetic(int a, int b, String operator) {
+        int result = 0;
+        switch (operator) {
+            case "add":
+                result = a + b;
+                break;
+            case "subtract":
+                result = a - b;
+                break;
+            case "multiply":
+                result = a * b;
+                break;
+            case "divide":
+                result = a / b;
+                break;
+        }
+        return result;
+    }
+}
