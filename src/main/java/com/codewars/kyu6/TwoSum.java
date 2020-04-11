4ab2a972c8f6949a760254a03704d2704dad71f2
nikolai belousov <belousov.nikolai@gmail.com>
2020-04-11 16:15:46 +0300

    Added solution for TwoSum kata

---
+++ src/main/java/com/codewars/kyu6/TwoSum.java
@@ -0,0 +1,14 @@
+package com.codewars.kyu6;
+
+public class TwoSum {
+    public static int[] twoSum(int[] numbers, int target) {
+        for (int i = 0; i < numbers.length; i++) {
+            for (int j = i + 1; j < numbers.length; j++) {
+                if (numbers[i] + numbers[j] == target) {
+                    return new int[] {i, j};
+                }
+            }
+        }
+        throw new IllegalArgumentException("There are no numbers for given target");
+    }
+}
