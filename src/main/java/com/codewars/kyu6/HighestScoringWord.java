83858933fa6151e4a148900fbc4b21794a2c3e10
nikolai belousov <belousov.nikolai@gmail.com>
2020-04-12 01:51:33 +0300

    Added solution for HighestScoringWord kata

---
+++ src/main/java/com/codewars/kyu6/HighestScoringWord.java
@@ -0,0 +1,11 @@
+package com.codewars.kyu6;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class HighestScoringWord {
+    public static String high(String s) {
+        return Arrays.stream(s.split(" "))
+                .max(Comparator.comparing(word -> word.chars().map(i -> i - 96).sum())).get();
+    }
+}
