9daa3330f2e48f931ae10d16ec337e3d78764eb0
nikolai belousov <belousov.nikolai@gmail.com>
2020-04-12 14:19:28 +0300

    Added solution for FoldingYourWayToTheMoon kata

---
+++ src/main/java/com/codewars/kyu7/FoldingYourWayToTheMoon.java
@@ -0,0 +1,15 @@
+package com.codewars.kyu7;
+
+public class FoldingYourWayToTheMoon {
+    public static Long fold(Double distance) {
+        double pageThickness = 0.0001;
+        long fold = 0;
+
+        if (distance < 0) return null;
+        while (pageThickness < distance) {
+            pageThickness *= 2;
+            fold++;
+        }
+        return fold;
+    }
+}
