735403f464756f45f8eb1ed0d5e02ab3f7eb6f1f
nikolai belousov <belousov.nikolai@gmail.com>
2020-04-12 14:39:19 +0300

    Modified solution for ExesAndOhs kata

---
+++ src/main/java/com/codewars/kyu7/ExesAndOhs.java
@@ -0,0 +1,19 @@
+package com.codewars.kyu7;
+
+public class ExesAndOhs {
+    public static boolean getXO (String str) {
+        String[] letters = str.toLowerCase().split("");
+        int x = 0;
+        int o = 0;
+
+        for (String letter : letters) {
+            if (letter.equals("x")) {
+                x++;
+            } else if (letter.equals("o")) {
+                o++;
+            }
+        }
+
+        return x == o;
+    }
+}
