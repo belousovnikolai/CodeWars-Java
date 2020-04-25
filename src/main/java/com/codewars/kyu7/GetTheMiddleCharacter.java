6ceba9ec1ebb7ea1abd0c40c0645c44a96710eb3
belousov <belousov.nikolai@gmail.com>
2020-04-26 01:30:39 +0300

    Added solution for 'Get The Middle Character' kata

---
+++ src/main/java/com/codewars/kyu7/GetTheMiddleCharacter.java
@@ -0,0 +1,12 @@
+package com.codewars.kyu7;
+
+public class GetTheMiddleCharacter {
+    public static String getMiddle(String word) {
+        int len = word.length();
+
+        if (len % 2 != 0) {
+            return word.substring(len / 2, len / 2 + 1);
+        }
+        return word.substring(len / 2 - 1, len / 2 + 1);
+    }
+}
