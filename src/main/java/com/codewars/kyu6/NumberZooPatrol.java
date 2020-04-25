31ebdebd3eaf3ef0d98c24e5141c8db1b03b675f
belousov <belousov.nikolai@gmail.com>
2020-04-25 16:35:39 +0300

    Added solution for 'NumberZooPatrol' kata

---
+++ src/main/java/com/codewars/kyu6/NumberZooPatrol.java
@@ -0,0 +1,12 @@
+package com.codewars.kyu6;
+
+public class NumberZooPatrol {
+    public static int findMissingNumber(int[] numbers) {
+        int result = 0;
+
+        for (int i = 0; i < numbers.length; i++) {
+            result += (i + 1) - numbers[i];
+        }
+        return result + numbers.length + 1;
+    }
+}
