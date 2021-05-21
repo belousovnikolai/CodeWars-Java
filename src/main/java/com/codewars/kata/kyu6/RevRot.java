47bb54d93a2999b554bf6dd19caba6712d98b181
belousov <belousov.nikolai@gmail.com>
2021-05-21 12:14:15 +0300

    Completed 'Reverse or rotate?' kata

---
+++ src/main/java/com/codewars/kata/kyu6/RevRot.java
@@ -0,0 +1,35 @@
+package com.codewars.kata.kyu6;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+class RevRot {
+    public static String revRot(String strng, int sz) {
+        if (sz <= 0 || sz > strng.length()) return "";
+
+        ArrayList<String> chunks = new ArrayList<>();
+
+        for (int i = 0; i < strng.length() / sz; i++) {
+            String chunk = strng.substring(i * sz, (i + 1) * sz);
+            int sum = Arrays.stream(chunk.split(""))
+                    .mapToInt(Integer::valueOf)
+                    .map(d -> d * d * d)
+                    .sum();
+
+            if (sum % 2 == 0) {
+                chunks.add(reverse(chunk));
+            } else {
+                chunks.add(rotate(chunk));
+            }
+        }
+        return String.join("", chunks);
+    }
+
+    private static String reverse(String str) {
+        return new StringBuilder(str).reverse().toString();
+    }
+
+    private static String rotate(String str) {
+        return str.substring(1) + str.substring(0, 1);
+    }
+}
